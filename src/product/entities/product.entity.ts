7c2bf773e8b24b5fb1fc9a12c9f8066767f42785
SitoraRashidova <sitorarashidova2024@gmail.com>
2025-01-16 12:04:25 +0500

    Like and Review tables created and finished their CRUD. Like, Review between Customer and Product tables connection successfully connected

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -7,6 +7,8 @@ import {
   OneToMany,
 } from 'typeorm';
 import { OrderDetail } from '../../order_detail/entities/order_detail.entity';
+import { Likes } from '../../like/entities/like.entity';
+import { Review } from '../../review/entities/review.entity';
 
 @Entity('product')
 export class Product {
@@ -48,4 +50,10 @@ export class Product {
 
   @OneToMany(() => OrderDetail, (orderDetail) => orderDetail.product)
   orderDetails: OrderDetail[];
+
+  @OneToMany(() => Likes, (like) => like.product)
+  likes: Likes[];
+
+  @OneToMany(() => Review, (review) => review.product)
+  reviews: Review[];
 }
