a46c03c15787d313ee60bfd5641198b55f0bf758
Shaxriyor <160018800+Shaxriyor0428@users.noreply.github.com>
2025-01-20 12:27:29 +0500

    Delete src/payme directory

--- src/payme/server/routes/payme.js
+++
@@ -1,9 +0,0 @@
-const paymeController = require('../controllers/payme.controller')
-const paymeMiddleware = require('../middlewares/payme.middleware')
-const userMiddleware = require('../middlewares/user.middleware')
-const router = require('express').Router()
-
-router.post('/pay', paymeMiddleware, paymeController.pay)
-router.post('/checkout', userMiddleware, paymeController.checkout)
-
-module.exports = router
