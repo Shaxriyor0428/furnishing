10df6b3eab01e34b60c73b39454c2e07b5d59d35
Shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2025-01-21 09:08:16 +0500

    rating has optimizated

--- src/rating/rating.module.ts
+++ src/rating/rating.module.ts
@@ -4,9 +4,11 @@ import { RatingController } from './rating.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Rating } from './entities/rating.entity';
 import { JwtModule } from '@nestjs/jwt';
+import { Product } from '../product/entities/product.entity';
+import { Customer } from '../customer/entities/customer.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Rating]), JwtModule],
+  imports: [TypeOrmModule.forFeature([Rating, Product, Customer]), JwtModule],
   controllers: [RatingController],
   providers: [RatingService],
 })
